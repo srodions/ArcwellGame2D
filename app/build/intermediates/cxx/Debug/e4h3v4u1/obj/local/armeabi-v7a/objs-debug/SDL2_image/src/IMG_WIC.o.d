d0ad02a5b143c67769c974e076fe2154c750861a
srodions <srodions53@gmail.com>
2026-03-09 14:49:44 +0300

    Удален мусор из сборки и обновлен .gitignore

--- app/build/intermediates/cxx/Debug/e4h3v4u1/obj/local/armeabi-v7a/objs-debug/SDL2_image/src/IMG_WIC.o.d
+++
@@ -1,2 +0,0 @@
-C:\lib\SDL2-2.32.10\android-project\app\build\intermediates\cxx\Debug\e4h3v4u1/obj/local/armeabi-v7a/objs-debug/SDL2_image/src/IMG_WIC.o: \
-  C:/lib/SDL2-2.32.10/android-project/app/jni/SDL2_image/src/IMG_WIC.c
